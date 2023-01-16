4df7ed537bf7d36ae610b6dfc8c3d1e63c853c2a
Sergey Khaykov <ssserg3000@gmail.com>
2023-01-16 13:55:24 +0200

    Initial commit

---
+++ src/Task4/Main.java
@@ -0,0 +1,26 @@
+package Task4;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Family> famList = new ArrayList<>();
+        famList.add(new Family("Kiev", "Sasha"));
+        famList.add(new Family("London", "Peter"));
+        famList.add(new Family("Paris", "Anton"));
+        famList.add(new Family("Kiev", "Anton"));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter city: ");
+        String city = sc.nextLine();
+
+        for (Family family : famList) {
+
+            if (family.city.equals(city)){
+                System.out.println(family.surname);
+            }
+
+        }
+
+    }
+}
